bba0bc3c51497740a183c1ca5c9a8e5f3c7a74d0
Marvin Russell Palabay <88232514+mpalabay@users.noreply.github.com>
2026-06-11 22:22:07 +0800

    create component and add title slot support

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    
+    <x-slot name="title">Home</x-slot>
+
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">Welcome to Pawi!</h1>
+                    <p class="mt-4 text-base-content/60">Pawi — A place to express your thoughts, share your stories,
+                        and let go of what weighs on your mind. Inspired by the
+                        Filipino word "pawi," meaning to ease, dispel, or release.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
